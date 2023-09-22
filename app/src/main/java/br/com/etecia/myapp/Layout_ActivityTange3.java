ce37fce6bf1227040ab39b09d8fd6759e1649417
Rhaulsal <rhaulsal2007@gmail.com>
2023-09-21 21:53:03 -0300

    PAG3-5

---
+++ app/src/main/java/br/com/etecia/myapp/Layout_ActivityTange3.java
@@ -0,0 +1,14 @@
+package br.com.etecia.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Layout_ActivityTange3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_tange2);
+    }
+}
